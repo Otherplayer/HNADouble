cdff53bbc2c73572fce655f729601854dcdb2469
__无邪_ <lel.guo@haihangyun.com>
2017-01-16 13:17:42 +0800

    for jenkins

---
+++ HNADouble/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HNADouble
+//
+//  Created by __无邪_ on 2017/1/16.
+//  Copyright © 2017年 __无邪_. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ HNADouble/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HNADouble
+//
+//  Created by __无邪_ on 2017/1/16.
+//  Copyright © 2017年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
